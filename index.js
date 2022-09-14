e5399abe3a4e7536166c8c75f7254af4fd1f266d
Brian Lambert <brianlambert2408@gmail.com>
2022-09-15 09:12:23 +1000

    commit tree 515223967b95ab26328d4945a56a1d1b83c5a932

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr)=> [arr[0], arr[1]];
+
+const returnLastTwoDrivers=(arr)=> [...arr.slice(-2)];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier =(num1)=> (num2)=> num1 * num2;
+
+const fareDoubler =(fare) => createFareMultiplier(fare)(2);
+
+const fareTripler =(fare) => createFareMultiplier(fare)(3);
+
+const selectDifferentDrivers=(arrDrivers, fn) => fn(arrDrivers);
